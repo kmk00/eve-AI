c7a31cd002c4d1cf578f762996d784ef776c26b9
kmk00 <73724318+kmk00@users.noreply.github.com>
2025-12-12 15:35:54 +0100

    feat(eve-client): add ResumeConversation component and related assets for AI character interaction
    * Added new files `char1.png`, `character_image.png`, `model1.png`, `model2.png` in the public folder
    * Created a new React functional component `ResumeConversation.tsx` in the components directory
    * Updated the index.tsx route to include the ResumeConversation component
    * Added a new style for buttons in styles.css
    
    This change aims to improve user interaction with the AI character by providing a visual representation and a button to initiate conversation.

---
+++ eve-client/src/components/ResumeConversation.tsx
@@ -0,0 +1,28 @@
+const ResumeConversation = () => {
+  const ai_name = "EVE";
+  const ai_image = "/character_image.png";
+
+  return (
+    <div className="group relative xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120  rounded-full">
+      <button
+        onClick={() => {}}
+        className="xl:w-120 md:w-100 md:h-100 w-80 h-80 rounded-full xl:h-120  relative"
+      >
+        <div
+          className={`xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120  bg-[url('${ai_image}')] bg-center bg-cover bg-no-repeat rounded-full`}
+        ></div>
+
+        <div className="xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120 top-0 left-0 absolute group-hover:bg-secondary-dark/40 bg-secondary-dark/0 transition-colors duration-200 rounded-full flex items-end justify-start md:pb-10 pl-10 pb-14 ">
+          <p className="opacity-0 group-hover:opacity-120 text-primary transition-opacity duration-200 xl:text-9xl md:text-7xl text-5xl">
+            {ai_name}
+          </p>
+        </div>
+      </button>
+
+      {/* TODO: VRM Model display */}
+      <div className="absolute -bottom-6 -right-6 w-40 h-40 bg-[url('/model1.png')] bg-center bg-cover bg-no-repeat rounded-full"></div>
+    </div>
+  );
+};
+
+export default ResumeConversation;
--- eve-client/src/routes/index.tsx
+++ eve-client/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import ConfigSettings from "@/components/ConfigSettings";
 import CurrentDate from "@/components/CurrentDate";
+import ResumeConversation from "@/components/ResumeConversation";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -9,9 +10,12 @@ export const Route = createFileRoute("/")({
 function App() {
   return (
     <div className="md:max-h-screen h-screen relative overflow-hidden">
-      <div>
-        <CurrentDate />
-        <ConfigSettings />
+      {/* <div> */}
+      <CurrentDate />
+      <ConfigSettings />
+      {/* </div> */}
+      <div className="flex items-center justify-center">
+        <ResumeConversation />
       </div>
     </div>
   );
