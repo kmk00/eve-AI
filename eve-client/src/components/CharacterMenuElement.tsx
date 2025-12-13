9aa78f06dc6988f92d6749b455fa8a17bc022029
kmk00 <73724318+kmk00@users.noreply.github.com>
2025-12-13 15:52:27 +0100

    feat(eve-client/src/components/CharacterMenuElement.tsx): introduce random clip-path shapes for CharacterMenuElement button
    The change introduces a new function `clipPath()` that generates a random polygon shape as a CSS clip-path property for the CharacterMenuElement button in the EVE client application. This adds visual variety to the button's appearance by changing its shape based on a randomly selected polygon configuration. The new clip-path shapes are hidden on smaller screens (below xl breakpoint) using media queries and the `after` pseudo-element.

--- eve-client/src/components/CharacterMenuElement.tsx
+++ eve-client/src/components/CharacterMenuElement.tsx
@@ -5,11 +5,32 @@ interface CharacterMenuElementProps {
 const CharacterMenuElement = ({
   name = "Character",
 }: CharacterMenuElementProps) => {
-  // TODO: ADD BACKROUND
+  const clipPath = () => {
+    const randomShape = Math.floor(Math.random() * 4) + 1;
+
+    switch (randomShape) {
+      case 1:
+        return "after:[clip-path:polygon(0%_0%,_100%_15%,_90%_100%,_10%_85%)]";
+      case 2:
+        return "after:[clip-path:polygon(10%_10%,_100%_0%,_85%_100%,_0%_90%)]";
+      case 3:
+        return "after:[clip-path:polygon(0%_5%,_95%_0%,_100%_85%,_15%_100%)]";
+      case 4:
+        return "after:[clip-path:polygon(5%_0%,_90%_10%,_100%_95%,_0%_100%)]";
+    }
+  };
 
   return (
     <button className="flex xl:w-fit xl:flex-col flex-row xl:items-end items-center justify-around xl:justify-start w-full">
-      <p className="text-secondary-dark font-expose xl:font-bold xl:ml-auto xl:text-xl text-4xl uppercase font-black">
+      <p
+        className={
+          `text-secondary-dark font-expose xl:font-bold xl:ml-auto xl:text-xl text-4xl uppercase font-black relative z-10
+        after:absolute after:left-[50%] after:bottom-0 after:-translate-x-[50%] after:translate-y-[5%] after:-z-10 after:xl:hidden
+        after:h-[1em] after:w-[3em] after:bg-secondary after:rotate-6
+        /* 2. Wycinamy kształt (x y) dla 4 punktów: Lewy-Góra, Prawy-Góra, Prawy-Dół, Lewy-Dół */
+        ` + clipPath()
+        }
+      >
         {name}
       </p>
       <div className="-skew-x-12 shadow-[-10px_10px_0px_-2px_#04233b] hover:shadow-[-10px_10px_0px_-2px_#0caff7] transform duration-200 overflow-hidden w-40 h-40 transform-gpu">
