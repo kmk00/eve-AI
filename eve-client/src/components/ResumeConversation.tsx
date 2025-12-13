78f16619cf8defed66ea71d534426a8883a4898e
kmk00 <73724318+kmk00@users.noreply.github.com>
2025-12-13 12:56:34 +0100

    feat(eve-client): update component ResumeConversation and styles.css to use new font paths and improve responsiveness
    * Update the ResumeConversation component to support larger sizes on 2xl screens
    * Update the styles.css file to use the new font paths for SundayMasthead and Expose fonts
    * Improve responsiveness of the ResumeConversation component by adjusting padding and height based on screen size

--- eve-client/src/components/ResumeConversation.tsx
+++ eve-client/src/components/ResumeConversation.tsx
@@ -3,16 +3,17 @@ const ResumeConversation = () => {
   const ai_image = "/character_image.png";
 
   return (
-    <div className="xl:-mt-14 group relative xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120 rounded-full">
+    <div className="xl:-mt-14 group relative xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120 2xl:w-150 2xl:h-150 rounded-full">
       <button
         onClick={() => {}}
-        className="xl:w-120 md:w-100 md:h-100 w-80 h-80 rounded-full xl:h-120  relative"
+        className="xl:w-120 md:w-100 md:h-100 w-80 h-80 rounded-full xl:h-120 2xl:w-150 2xl:h-150  relative"
       >
         <div
-          className={`xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120  bg-[url('${ai_image}')] bg-center bg-cover bg-no-repeat rounded-full`}
+          className="xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120 2xl:w-150 2xl:h-150 bg-center bg-cover bg-no-repeat rounded-full"
+          style={{ backgroundImage: `url(${ai_image})` }}
         ></div>
 
-        <div className="xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120 top-0 left-0 absolute group-hover:bg-secondary-dark/40 bg-secondary-dark/0 transition-colors duration-200 rounded-full flex items-end justify-start md:pb-10 pl-10 pb-14 ">
+        <div className="xl:w-120 md:w-100 md:h-100 w-80 h-80 xl:h-120 2xl:w-150 2xl:h-150 top-0 left-0 absolute group-hover:bg-secondary-dark/40 bg-secondary-dark/0 transition-colors duration-200 rounded-full flex items-end justify-start md:pb-10 2xl:pb-32 pl-10 pb-14 ">
           <p className="opacity-0 group-hover:opacity-120 text-primary transition-opacity duration-200 xl:text-9xl md:text-7xl text-5xl">
             {ai_name}
           </p>
