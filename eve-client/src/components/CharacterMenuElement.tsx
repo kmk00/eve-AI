7d15a456adbae7f64e320b6823e499bcca373bd5
kmk00 <73724318+kmk00@users.noreply.github.com>
2025-12-13 14:19:28 +0100

    feat: add character menu elements for Eleven, Riko, and Eve in the app layout
    * Added a new SVG file `circle-b.svg` for decorative purposes
    * Updated `CharacterMenuElement` component to display names of characters (Eleven, Riko, and Eve)
    * Modified the app layout to include character menu elements and moved the "Add New Character" button to a more appropriate location
    * Added a new image element to display the decorative SVG file on larger screens
    * Removed the unused div in the app layout

--- eve-client/src/components/CharacterMenuElement.tsx
+++ eve-client/src/components/CharacterMenuElement.tsx
@@ -9,7 +9,7 @@ const CharacterMenuElement = ({
 
   return (
     <button className="flex xl:w-fit xl:flex-col flex-row xl:items-end items-center justify-around xl:justify-start w-full">
-      <p className="text-secondary-dark font-expose xl:font-bold xl:ml-auto xl:text-xl text-6xl uppercase font-black">
+      <p className="text-secondary-dark font-expose xl:font-bold xl:ml-auto xl:text-xl text-4xl uppercase font-black">
         {name}
       </p>
       <div className="-skew-x-12 shadow-[-10px_10px_0px_-2px_#04233b] hover:shadow-[-10px_10px_0px_-2px_#0caff7] transform duration-200 overflow-hidden w-40 h-40 transform-gpu">
--- eve-client/src/routes/index.tsx
+++ eve-client/src/routes/index.tsx
@@ -12,35 +12,40 @@ export const Route = createFileRoute("/")({
 
 function App() {
   return (
-    <div className="md:max-h-screen h-screen relative xl:overflow-y-hidden overflow-x-hidden">
-      <CurrentDate />
-      <ConfigSettings />
-      <div className="flex flex-col gap-12 items-center justify-center">
-        <ResumeConversation />
-        <div className="xl:hidden block">
+    <>
+      <div className="h-screen relative xl:overflow-y-hidden overflow-x-hidden">
+        <CurrentDate />
+        <ConfigSettings />
+        <div className="flex flex-col gap-12 items-center justify-center">
+          <ResumeConversation />
+          <div className="xl:hidden block">
+            <CharacterListButton />
+          </div>
+        </div>
+        <div className="hidden xl:block absolute right-6 top-[20%] transform-y-[50%]">
+          <div className="flex flex-col items-end gap-4 mb-16">
+            <CharacterMenuElement name="Eleven" />
+            <CharacterMenuElement name="Riko" />
+            <CharacterMenuElement name="Eve" />
+          </div>
           <CharacterListButton />
         </div>
-      </div>
-      <div className="hidden xl:block absolute right-6 top-[20%] transform-y-[50%]">
-        <div className="flex flex-col items-end gap-4 mb-8">
+        <div className="hidden xl:block">
+          <AddNewCharacter />
+        </div>
+        <div className="flex xl:hidden flex-col items-end gap-4 mt-12 mb-8 px-4">
           <CharacterMenuElement name="Eleven" />
           <CharacterMenuElement name="Riko" />
           <CharacterMenuElement name="Eve" />
         </div>
-        <CharacterListButton />
+        <div className="xl:hidden flex flex-col items-center mt-20 ">
+          <AddNewCharacter />
+        </div>
+        <img
+          src="/circle-b.svg"
+          className="hidden xl:block m-auto xl:absolute xl:-bottom-4 left-[50%] translate-x-[-50%] "
+        ></img>
       </div>
-      <div className="hidden xl:block">
-        <AddNewCharacter />
-      </div>
-      <div className="flex xl:hidden flex-col items-end gap-4 mt-12 mb-8 px-4">
-        <CharacterMenuElement name="Eleven" />
-        <CharacterMenuElement name="Riko" />
-        <CharacterMenuElement name="Eve" />
-      </div>
-      <div className="xl:hidden flex flex-col items-center mt-20 ">
-        <AddNewCharacter />
-      </div>
-      {/* <div className="absolute bottom-0 left-[50%] translate-x-[-50%] translate-y-[95%] w-400 h-400 bg-secondary-dark rounded-full"></div> */}
-    </div>
+    </>
   );
 }
